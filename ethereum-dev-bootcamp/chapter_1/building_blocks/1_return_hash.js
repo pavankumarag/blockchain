5650a49b03bf1a9a18de22bd7471ec12848ef881
pavankumarag <pavankumar.ag@gmail.com>
2023-12-01 16:40:07 +0530

    cosmetics

--- ethereum-dev-bootcamp/chapter_1/building_blocks/1_return_hash.js
+++ ethereum-dev-bootcamp/chapter_1/building_blocks/1_return_hash.js
@@ -2,7 +2,7 @@ const SHA256 = require('crypto-js/sha256');
 
 class Block {
     toHash() {
-        return SHA256("pavan"); // a hash!
+        return SHA256("pavan"); // a simple way to show how to compute hash!
     }
 }
 
